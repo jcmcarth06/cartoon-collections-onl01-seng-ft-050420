fc4d8ea19466ac114694475fd6e1ec50641235ef
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-08 23:35:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ end
 def find_the_cheese(cheese_types)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese.collect do |name|
+  cheese_types.collect do |name|
     if cheese_types.include?(name)
       return "#{name}"
     end
