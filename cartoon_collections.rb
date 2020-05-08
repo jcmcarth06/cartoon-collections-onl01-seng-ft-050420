9c7b49e94e38b0b3c1abf337617cb7ed6256f291
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-08 21:45:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,7 @@ def summon_captain_planet(planeteer_calls)
 
   planeteer_calls.map { |fruits| fruits.capitalize }
   planeteer_calls.each { |fruits| puts fruits + "!"}
-  planeteer_calls.map { |veggies| veggies.upcase }
-  planeteer_alls.each { |veggies| puts veggies + "!"}
+  
 
   # code an argument here
   # Your code here
