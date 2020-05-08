0cdc755f01d2319e08415b1634be6e388b8815fa
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-08 22:45:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,11 +17,13 @@ def long_planeteer_calls(calls_long)
   calls_long.collect do |name|
     if name.length > 4
       return true
+    
     end
   end
+  return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese()# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
