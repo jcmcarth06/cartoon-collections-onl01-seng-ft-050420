9fe63baa350b6260a965df75fe8aa9a5ab5c6ec4
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-08 23:37:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,8 +29,7 @@ def find_the_cheese(cheese_types)# code an argument here
   cheese_types.collect do |name|
     if cheese_types.include?(name)
       return "#{name}"
-    end
-    nil
+    else nil  
   end
 
 end
